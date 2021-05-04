d7cf1cda263787e77074b863e7e3a4ef692607ee
num5268 <num5268@gmail.com>
2021-05-04 16:39:51 +0900

    DBMS

---
+++ Oracle/관리자(2021-05-04-kschoolDB생성).sql
@@ -0,0 +1,8 @@
+CREATE TABLESPACE KschoolDB
+DATAFILE 'C:/oraclexe/data/kschool.dbf'
+SIZE 1M AUTOEXTEND ON NEXT 1K;
+
+CREATE USER kscuser IDENTIFIED BY kscuser
+DEFAULT TABLESPACE KschoolDB;
+
+GRANT DBA TO kscuser;
