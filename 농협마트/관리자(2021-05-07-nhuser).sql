3e121ec05e623c08cc13a7302c38a3a507cd48d3
num5268 <num5268@gmail.com>
2021-05-07 16:40:04 +0900

    농협마트

---
+++ 농협마트/관리자(2021-05-07-nhuser).sql
@@ -0,0 +1,8 @@
+CREATE TABLESPACE nonghyupDB
+DATAFILE 'C:/oraclexe/data/nonghyup.dbf'
+SIZE 1M AUTOEXTEND ON NEXT 1K;
+
+CREATE USER nhuser IDENTIFIED BY nhuser
+DEFAULT TABLESPACE nonghyupDB;
+
+GRANT DBA TO nhuser;
